7d76e2ada767b1426d5a3e193e9bfe0b7e307b1b
Nancy McCollough <spacecolossi@gmail.com>
2020-02-23 16:59:05 -0800

    fixed username thing

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,8 @@ const router = express.Router();
 
 // this is our MongoDB database
 const dbRoute =
-    //'mongodb+srv://username:password@alphacluster-plrgq.mongodb.net/test?retryWrites=true&w=majority';
-  'mongodb+srv://nancy:serena@alphacluster-plrgq.mongodb.net/test?retryWrites=true&w=majority';
-
+    'mongodb+srv://username:password@alphacluster-plrgq.mongodb.net/test?retryWrites=true&w=majority';
+  
 // connects our back end code with the database
 mongoose.connect(dbRoute, { useNewUrlParser: true });
 
